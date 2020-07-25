af09962f8f0674afdc12e38811587a9c0efdf371
Paulo Lima <ph.lima1983@gmail.com>
2020-07-25 19:59:24 -0300

    commit inicial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '82de156176ca071e3e8f9ce3aade0d19'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7c4157f6-3e96-47ba-9e5e-f8d606591fe3'
+};
